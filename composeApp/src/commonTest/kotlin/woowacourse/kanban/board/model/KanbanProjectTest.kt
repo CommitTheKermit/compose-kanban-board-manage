bf3c839676c48c42e1e20c9e0a1c2712017f20f3
ujeong <qnf323@naver.com>
2026-03-24 17:23:18 +0900

    test: 스크의 상태를 변경 할 수 있어야 한다

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/model/KanbanProjectTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/model/KanbanProjectTest.kt
@@ -30,4 +30,24 @@ class KanbanProjectTest {
 
         assertEquals(1, project.tasks.size)
     }
+
+    @Test
+    fun `태스크의 상태를 변경 할 수 있어야 한다`() = runTest {
+        var task = KanbanTask(
+            data = BoardData(
+                title = Title("제목"),
+                content = "내용",
+                tags = Tags(),
+                nickname = Nickname("아오"),
+            ),
+            status = TaskStatus.IN_PROGRESS,
+        )
+
+        val project = KanbanProject(mutableListOf())
+        val state = BoardState(backgroundScope, project = project)
+
+        task = state.changeStatus(task, TaskStatus.DONE)
+
+        assertEquals(TaskStatus.DONE, task.status)
+    }
 }
