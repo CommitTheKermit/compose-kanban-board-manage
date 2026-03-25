66ceb32d1e466bf000c23c4460960821801d7f96
ujeong <qnf323@naver.com>
2026-03-25 17:35:10 +0900

    test: 프로젝트를 선택하면 프로젝트에 저장되어 있는 태스크들이 표시되어야 한다

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/StatusCardList.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/StatusCardList.kt
@@ -26,6 +26,7 @@ import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.boundsInWindow
 import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -137,7 +138,8 @@ fun StatusCardList(
             modifier = Modifier
                 .padding(vertical = 12.dp)
                 .fillMaxHeight()
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .testTag(status.displayName),
 
         ) {
             items(count = tasks.size, key = { tasks[it].data.id }) {
