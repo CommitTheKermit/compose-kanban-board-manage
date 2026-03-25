ac11eaa28d95d98933b57f8a3efcceee6f81b494
ujeong <qnf323@naver.com>
2026-03-25 17:44:22 +0900

    test: 프로젝트를 선택하면 보드의 제목이 변경되어야 한다

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/KanbanBoardHeader.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/KanbanBoardHeader.kt
@@ -20,6 +20,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -45,6 +46,7 @@ fun KanbanBoardHeader(
                 Text(
                     headerTitle,
                     fontSize = 24.sp,
+                    modifier = Modifier.testTag("headerTitle"),
                 )
                 Spacer(modifier = Modifier.height(4.5.dp))
                 Text(
--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/ui/KanbanProjectUiTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/ui/KanbanProjectUiTest.kt
@@ -1,7 +1,9 @@
 package woowacourse.kanban.board.ui
 
 import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.assert
 import androidx.compose.ui.test.assertCountEquals
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.onChildren
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
@@ -27,5 +29,22 @@ class KanbanProjectUiTest {
         onNodeWithTag("In Progress")
             .onChildren()
             .assertCountEquals(3)
+    }
+
+    @Test
+    fun `프로젝트를 선택하면 보드의 제목이 변경되어야 한다`() = runComposeUiTest {
+        // given : 칸반 페이지가 주어지고 프로젝트 리스트는 칸반 페이지 내부에 MockData로 설정 되어 있다
+        setContent {
+            KanbanPage()
+        }
+
+        // when : 가장 처음 프로젝트의 제목이 표시되고 다른 프로젝트 버튼을 눌렀을 때
+        onNodeWithTag("headerTitle").assert(hasText("Compose1"))
+
+        onNodeWithText("Compose2").performClick()
+        waitForIdle()
+
+        // then : 보드의 제목이 선택된 프로젝트의 이름으로 변경되어야 한다
+        onNodeWithTag("headerTitle").assert(hasText("Compose2"))
     }
 }
