94d21a30a7d6d0eebce99d3b1b1ea29b90b277d6
ujeong <qnf323@naver.com>
2026-03-25 14:31:29 +0900

    refactor: StatusCardList 공통 코드 제거

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/model/BoardData.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/model/BoardData.kt
@@ -1,3 +1,5 @@
 package woowacourse.kanban.model
 
-data class BoardData(val title: Title, val content: String = "", val tags: Tags, val nickname: Nickname)
+data class BoardData(val title: Title, val content: String = "", val tags: Tags, val nickname: Nickname) {
+    val id: Long = System.currentTimeMillis()
+}
